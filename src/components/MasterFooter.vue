ea9a40895d94e2c8934bc54640375951d45d3d12
Valerio <canivalerio93@gmail.com>
2022-09-06 16:11:31 +0200

    Fine Inserimento Elementi. Ora si parte con la stilizzazione

--- src/components/MasterFooter.vue
+++ src/components/MasterFooter.vue
@@ -1,6 +1,37 @@
 <template>
   <footer>
-    <div class="container"></div>
+    <div class="container">
+
+      <div class="footer_column">
+          <h2>{{About.headline}}</h2>
+          <p>{{About.content}}</p>
+          <i v-for="(icon, index) in About.social" :key="index" class="fa-brands" :class="'fa-'+icon"></i>
+      </div>
+
+      <div class="footer_column">
+          <h2>{{Contact.headline}}</h2>
+          <p v-for="(contact, index) in Contact.content" :key="index">{{contact}}</p>
+      </div>
+
+      <div class="footer_column">
+          <h2>{{Pages.headline}}</h2>
+          <ul>
+            <li v-for="(page, index) in Pages.content" :key="index">{{page}}</li>
+          </ul>
+      </div>
+
+      <div class="footer_column">
+          <h2>{{Blog.headline}}</h2>
+          <div v-for="(article, index) in Blog.content" :key="index">
+            <img :src="require('@/assets/img/' + article.img)" alt="">
+            <div>
+              <p>{{article.text}}</p>
+              <span>{{article.date}}</span>
+            </div>
+          </div>
+      </div>
+
+    </div>
   </footer>
 </template>
 
@@ -29,7 +60,7 @@ export default {
           'info@masterstudy.com'
         ]
       },
-      
+
       Pages:{
         headline:'PAGES',
         content:[
@@ -46,12 +77,12 @@ export default {
         headline:'BLOG',
         content:[
           {
-            img:'',
+            img:'Article1.jpeg',
             text:'Our main target is to "Developing Yourself as a Leader"',
             date:'--August 9, 2018',
           },
           {
-            img:'',
+            img:'Article2.jpeg',
             text:'Those Other College Expenses You Aren\'t Thinking About',
             date:'--June 3, 2015',
           },
