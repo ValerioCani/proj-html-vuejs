942e42ec0cda5a19548fbac7130f8795e9f34365
Valerio <canivalerio93@gmail.com>
2022-09-06 15:33:03 +0200

    scrittura data del footer

--- src/components/MasterFooter.vue
+++ src/components/MasterFooter.vue
@@ -8,18 +8,54 @@
 export default {
   name:'MasterFooter',
   data(){
-    return{
+    return{   
+
       About:{
         headline:'ABOUT',
         content:'Masterstudy is Educatio WordPres Theme Featured By Learning Management System (LMS) for online education. Developed by StylemixThemes.',
         social:[
-          facebook,
-          twitter,
-          instagram
+          'facebook',
+          'twitter',
+          'instagram'
         ]
       },
+
       Contact:{
         headline:'CONTACT',
+        content:[
+          'USA, California 20, First Avenue, California',
+          'Tel.: +1 212 458 300 32',
+          'Fax: +1 212 375 24 14',
+          'info@masterstudy.com'
+        ]
+      },
+      
+      Pages:{
+        headline:'PAGES',
+        content:[
+        'Blog',
+        'Courses',
+        'Home',
+        'Membership',
+        'Shortcodes',
+        'Typography',
+        ]
+      },
+
+      Blog:{
+        headline:'BLOG',
+        content:[
+          {
+            img:'',
+            text:'Our main target is to "Developing Yourself as a Leader"',
+            date:'--August 9, 2018',
+          },
+          {
+            img:'',
+            text:'Those Other College Expenses You Aren\'t Thinking About',
+            date:'--June 3, 2015',
+          },
+        ]
       }
     } 
   }
