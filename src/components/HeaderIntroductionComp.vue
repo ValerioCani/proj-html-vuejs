a299ff063c420183ac21cc91a6698ffd66bb0c05
Valerio <canivalerio93@gmail.com>
2022-09-07 11:22:08 +0200

    fine stilizzazione header

--- src/components/HeaderIntroductionComp.vue
+++ src/components/HeaderIntroductionComp.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="container">
-        <div>
+    <div class="container flex">
+        <div class="intro_column1">
             <h2>Hey there, my name is Jhon Doe. I'm the founder of the Brain Academy.</h2>
             <img src="../assets/img/sign.jpg" alt="">
         </div>
-        <div>
+        <div class="intro_column2">
             <p>A wonderful serenity has taken possessionof my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss do souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my tslents. When, while the lovely valley teems with vapour around me, and the meridian sun stikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary</p>
         </div>
     </div>
@@ -17,5 +17,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.container{
+    padding: 200px 0 100px 0;
+    .intro_column1{
+        width: 40%;
+        h2{
+            font-size: 38px;
+        }
+    }
+    .intro_column2{
+        width: 60%;
+        padding-left: 70px;
+        h2{
+            font-size: 40px;
+        }
+    }
+    
+    p{
+            line-height: 40px;
+        }
+}
 
 </style>
--- src/components/HeaderJumboTron.vue
+++ src/components/HeaderJumboTron.vue
@@ -8,12 +8,15 @@
             <div id="course_sale" class="flex">
                 <div>
                     <h2><span class="secondary_text">Limited Sale:</span> All courses with 55% off</h2>
-                    <input type="email">
-                    <button>SUBSCRIBE</button>
+                    <div class="flex">
+                        <input type="email" placeholder="Enter Your E-mail">
+                        <button class="button header_button secondary_text">SUBSCRIBE</button>
+                    </div>
+               
                 </div>
                 <div class="flex">
                     <div class="time">
-                        <div class="num">26</div>
+                        <div class="num ">26</div>
                         <div class="unit">Days</div>
                     </div>
                     <div class="time">
@@ -64,19 +67,45 @@ export default {
                 }
             }
             #course_sale{
-                width: 1310px;
+                // width: 1310px;
                 height: 242px;
                 background-color: $primary_color;
                 border-radius: 10px;
                 position: absolute;
                 top: 670px;
+                left: 50%;
+                transform: translate(-50%);
                 justify-content: center;
                 align-items: center;
+                padding: 50px;
                 h2{
                     font-size: 30px;
+                    margin: 25px 0;
+                }
+                input{
+                    height: 60px;
+                    width: 490px;
+                    padding: 3%;
+                    border: none;
+                    border-radius: 30px;
+                }
+                .header_button{
+                    position:relative;
+                    right: 55px;
+                    background-color: #2D4649;
                 }
                 .time{
-                
+                    padding: 25px;
+                    .num{
+                        font-size: 70px;
+                        font-weight: bold;
+                        color: $secondary_text_color;
+                    }
+                    .unit{
+                        padding: 10px;
+                        font-size: 20px;
+                        font-weight: bold;
+                    }
                 }
             }
         }
